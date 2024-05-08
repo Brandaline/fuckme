e14df7db6b34084ae97eca84e7136bdb90be8564
Brandaline <64507730+Brandaline@users.noreply.github.com>
2024-05-08 04:20:30 -0500

    Add files via upload

--- bitch/firewall_log_analysis.sh
+++ bitch/firewall_log_analysis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+
 # Ensure a log file is provided
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <firewall_log_file>"
@@ -20,9 +20,7 @@ fi
 # to create a pipline that will display. Use filters to count the DENY attempts
 # and sort them by numerical value and list them in a descending order
 echo "Denied Access Attempts:"
-awk '$2 == "DENY" {print $3}' "$log_file" | sort | uniq -c | sort -rn | cat -n
-awk '$2 == "DENY" {print $3}' "$log_file" > denied_ips.txt
-cat denied_ips.txt
+awk '/DENY/ {print $6}' "$log_file" | sort | uniq -c | sort -rn 
 
 
 #########################################################################
@@ -31,10 +29,7 @@ cat denied_ips.txt
 # count them. Finally sort the attempts by numerical value and list them in discending order
 # if they are above 5
 echo "Frequent Access Attempts (more than 5 attempts):"
-awk '$2 == "DENY" {print $3}' "$log_file" | sort | uniq -c | awk '$1 > 5 {print $2}' | cat -n
-awk '$2 == "DENY" {print $3}' "$log_file" > denied_ips.txt
-cat denied_ips.txt
-
+awk '/DENY/ {print $6}' "$log_file" | sort | uniq -c | awk '$1 > 5 {print $2}' | cat -n
 
 #########################################################################
 # Task 4: Detect Known Malicious Signatures
@@ -47,7 +42,7 @@ grep -E '10\.10\.10\.(10|20)' "$log_file"
 # create a variable called total_attempts that stores the  number of the attempts 
 # create a variable called unique_denied_ips that stores the number of denied attempts
 total_attempts=$(wc -l < "$log_file")
-unique_denied_ips=$(sed -n '/DENY/ s/.* \([0-9]*\).*/\1/p' "$log_file" | sort -u | wc -l)
+unique_denied_ips=$(awk '/DENY/ {print $6}' "$log_file" | sort -u | wc -l)
 
 #########################################################################
 # Print the summary report of the total and the denied attemps
